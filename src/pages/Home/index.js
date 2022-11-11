17f518d8e90cca47652c0b6a551e09a73bb969b4
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-12 00:53:49 +0100

    routes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Clients from "../../components/Clients";
+import Footer from "../../components/Footer";
+import { Header } from "../../components/Header";
+import Hero from "../../components/Hero";
+import Quote from "../../components/Quote";
+import Navbar from "../../components/Navbar";
+import Nft from "../../components/Nft";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Clients />
+      <Quote />
+      <Nft />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
